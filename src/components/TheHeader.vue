812611c71972c2fc57264be7d52f971a07596ac2
OziOcb <pozyzniewski@yahoo.com>
2022-07-11 12:58:35 +0100

    Add styles for TheHeader

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,26 +1,35 @@
 <template>
-  <header>
-    <h1>Logo</h1>
+  <header class="header">
+    <LogoSpotify class="header__logo" />
 
-    <button v-if="!userData.name" @click="handleLogIn()">
-      Log in with Spotify
-    </button>
+    <div class="header__content">
+      <BaseButton v-if="!userData.name" @click="handleLogIn()">
+        Log in with Spotify
+      </BaseButton>
 
-    <div v-else>
-      Hi
-      <a :href="userData.spotifyUrl" target="_blank" rel="noopener noreferrer">
-        {{ userData.name }}
-      </a>
+      <p v-else>
+        Hi
+        <a
+          :href="userData.spotifyUrl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {{ userData.name }}
+        </a>
+      </p>
     </div>
   </header>
 </template>
 
 <script>
 import SpotifyService from "@/services/spotify-service";
+import LogoSpotify from "@/components/icons/LogoSpotify.vue";
+import BaseButton from "@/components/BaseButton.vue";
 import { mapState } from "pinia";
 import { useSpotifyStore } from "@/stores/spotifyStore";
 
 export default {
+  components: { LogoSpotify, BaseButton },
   computed: {
     ...mapState(useSpotifyStore, ["userData"]),
   },
@@ -33,4 +42,31 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+
+  @media (min-width: $breakpoint-md) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  &__logo {
+    width: 200px;
+  }
+
+  &__content {
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+
+    a {
+      color: $color-text-primary;
+      font-weight: bold;
+    }
+  }
+}
+</style>
